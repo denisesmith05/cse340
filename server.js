26ff33d839fc827c77460937b252fcb4adae86a1
Denise Smith <denise.sunshine@icloud.com>
2026-09-02 18:01:11 -0600

    change world to express

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from World!');
+  res.send('Hello from Express!');
 });
 
 app.listen(PORT, () => {
