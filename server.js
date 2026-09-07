f3fc50a1ea943384639f3a8000d88fa6968d9758
Denise Smith <denise.sunshine@icloud.com>
2026-09-07 16:07:29 -0600

    converting html files to ejs

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 import express from 'express';
-
 import { fileURLToPath } from 'url';
 import path from 'path';
 
 //Define the application environment
-const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'development';
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
 
 //Define the port number the server will listen on
 const PORT = process.env.PORT || 3000;
 
-const app = express();
-
 //Define the __filename and __dirname variables for ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const app = express();
 
 /**
   * Configure Express middleware
@@ -22,19 +21,27 @@ const __dirname = path.dirname(__filename);
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tell Express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
 /**
   * Routes
   */
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/home.html'));
+app.get('/', async (req, res) => {
+    const title = 'Home';
+    res.render('home', { title });
 });
 
-app.get('/organizations', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/organizations.html'));
+app.get('/organizations', async (req, res) => {
+    const title = 'Organizations';
+    res.render('organizations', { title });
 });
 
-app.get('/projects', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/projects.html'));
+app.get('/projects', async (req, res) => {
+    const title = 'Projects';
+    res.render('projects', { title });
 });
 
 app.listen(PORT, () => {
