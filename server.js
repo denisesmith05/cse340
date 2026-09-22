36e3e114fd6d7c831b02545c40e77c6a806e49ff
Denise Smith <denise.sunshine@icloud.com>
2026-09-21 23:12:42 -0600

    added async and await function to app.listen

--- server.js
+++ server.js
@@ -50,8 +50,9 @@ app.get('/categories', async (req, res) => {
     res.render('categories', { title });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     try {
+        await testConnection();
         console.log(`Server is running at http://127.0.0.1:${PORT}`);
         console.log(`Environment: ${NODE_ENV}`);
     } catch (error) {
