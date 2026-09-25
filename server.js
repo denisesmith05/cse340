df271e8d3c14c3d7e32aea83d1659c5cf98a5b83
Denise Smith <denise.sunshine@icloud.com>
2026-09-25 16:09:19 -0600

    altered get function to categories to call getAllCategories

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import path from 'path';
 import { testConnection } from './src/models/db.js';
 import { getAllOrganizations } from './src/models/organizations.js';
 import { getAllProjects } from './src/models/projects.js';
+import { getAllCategories } from './src/models/categories.js';
 
 
 //Define the application environment
@@ -50,16 +51,16 @@ app.get('/organizations', async (req, res) => {
 
 app.get('/projects', async (req, res) => {
     const projects = await getAllProjects();
-    
-    res.render('projects', {
-        title: 'Service Projects',
-        projects
-    });
+    const title = 'Projects';
+
+    res.render('projects', {title, projects});
 });
 
 app.get('/categories', async (req, res) => {
+    const categories = await getAllCategories();
     const title = 'Categories';
-    res.render('categories', { title });
+
+    res.render('categories', { title, categories });
 });
 
 app.listen(PORT, async () => {
